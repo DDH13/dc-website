0055fd32a32d9b8d58231364f2d11bcb86a13acd
Dineth Hettiarachchi <dinethhettiarachchi5@gmail.com>
2024-02-02 21:31:28 +0530

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,31 +1,16 @@
 import React from 'react';
-import { ThemeProvider, CssBaseline, Container, Typography, Box } from '@mui/material';
-import { createTheme } from '@mui/material/styles';
+import {ThemeProvider, Box, CssBaseline, Container} from '@mui/material';
+import {createTheme} from '@mui/material/styles';
+import {FaFacebookF, FaInstagram, FaYoutube, FaTwitter, FaEnvelope} from 'react-icons/fa';
+import styled from "styled-components";
 
-const defaultTheme = createTheme({
-    // Your theme customization
-});
+const defaultTheme = createTheme({});
 const StickyFooter = () => {
     return (
         <ThemeProvider theme={defaultTheme}>
-            <Box
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    minHeight: '100vh',
-                }}
-            >
-                <CssBaseline />
-                <Container component="main" sx={{ mt: 8, mb: 2 }} maxWidth="sm">
-                    <Typography variant="h2" component="h1" gutterBottom>
-                        Sticky footer
-                    </Typography>
-                    <Typography variant="h5" component="h2" gutterBottom>
-                        {'Pin a footer to the bottom of the viewport.'}
-                        {'The footer will move as the main element of the page grows.'}
-                    </Typography>
-                    <Typography variant="body1">Sticky footer placeholder.</Typography>
-                </Container>
+
+                <CssBaseline/>
+
                 <Box
                     component="footer"
                     sx={{
@@ -38,14 +23,67 @@ const StickyFooter = () => {
                                 : theme.palette.grey[800],
                     }}
                 >
-                    <Container maxWidth="sm">
-                        <Typography variant="body1">
-                            My sticky footer can be found here.
-                        </Typography>
+                    <Container className="row"
+                               style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '1em'}}>
+                        <div className="col-5">
+                            <small>© Copyright 2023. Dineth and Himaya</small>
+                        </div>
+                        <div className="col-5" >
+                            <SocialLinks links={socialLinks}/>
+                        </div>
                     </Container>
                 </Box>
-            </Box>
+
         </ThemeProvider>
+    );
+};
+const socialLinks = [
+    {href: "https://www.youtube.com/@debaterscouncil-srilanka6367", icon: 'FaYoutube', name: 'YouTube', color: 'red'},
+    {href: "https://www.facebook.com/DebatersCouncilSriLanka/", icon: 'FaFacebookF', name: 'Facebook', color: 'blue'},
+    {href: "https://www.instagram.com/debaterscouncilsl/", icon: 'FaInstagram', name: 'Instagram', color: 'purple'},
+    {href: "https://twitter.com/DebatersCouncil", icon: 'FaTwitter', name: 'Twitter', color: 'skyblue'},
+    {href: "mailto:debaterscouncil@gmail.com", icon: 'FaEnvelope', name: 'Email', color: 'lightred'},
+];
+
+const icons = {
+    FaYoutube,
+    FaFacebookF,
+    FaInstagram,
+    FaTwitter,
+    FaEnvelope
+};
+
+// Styled components
+const List = styled.ul`
+  list-style: none;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ListItem = styled.li`
+  margin: 0 10px;
+`;
+
+const FeedbackLink = styled.a`
+  margin-left: 50px;
+  margin-bottom: -15px;
+  font-size: 20px;
+`;
+
+// Component
+const SocialLinks = ({links}) => {
+    return (
+        <List>
+            {links.map(({href, icon, name, color}, index) => (
+                <ListItem key={index}>
+                    <a href={href} target="_blank" rel="noopener noreferrer">
+                        {React.createElement(icons[icon], {style: {color: color, height: '20px', width: '20px'}})}
+                    </a>
+                </ListItem>
+            ))}
+        </List>
     );
 };
 
